d4e48135347f5311f7f7f1d24357134d4eb724d9
Mario Netto <netto.infor@gmail.com>
2014-08-05 17:59:01 -0300

    matriz-c
    Códigos úteis pra quem programa em C

---
+++ multi-dimensional-arrays/basic-multi-dimensional-arrays.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+	int matriz [4][4], i, j;
+
+	for (i=0; i<4; i++){
+		for (j=0; j<4; j++){
+			printf ("Informe valor [linha %d] [Coluna %d]\n", i+1, j+2);
+			scanf ("%d", &matriz[i][j]);
+		}
+	}
+
+	printf ("\n");
+
+	for (i=0; i<4; i++){
+		for (j=0; j<4; j++){
+			printf ("%d ", matriz[i][j]);
+		}
+		printf ("\n");
+	}
+
+	printf ("\n");
+
+	system ("pause");
+	return 0;
+}
